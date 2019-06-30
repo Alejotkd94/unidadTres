e81154c80c98388bb64ab49b25e5b1e0409a3af2
Alejandro Arbelaez <alejo.tkd94@live.com>
2019-06-30 16:39:18 -0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
 process.env.PORT = process.env.PORT || 3000;
-process.env.URLBD = 'mongodb+srv://darbelaez:Tauro1956@nodejstdea-mboaa.mongodb.net/CursosExtencion?retryWrites=true&w=majority';
+process.env.URLBD = 'mongodb://localhost:27017/CursosExtencion';
